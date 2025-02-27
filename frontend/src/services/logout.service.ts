12a8771ee6d75ba8a33c87493d262540dace8d78
Sarawut2543 <sarawut27git@gmail.com>
2025-02-27 15:42:44 +0700

    Update logout.service.ts

--- frontend/src/services/logout.service.ts
+++ frontend/src/services/logout.service.ts
@@ -1,10 +1,10 @@
 import { LOGOUT } from "@/apis/endpoint.api";
 import mainApi from "@/apis/main.api";
 import { LogoutResponse } from "@/types/response/response.logout";
-import { playlodeLogout } from "@/types/requests/request.logout";
+import { PayloadLogout } from "@/types/requests/request.logout";
 
-export const logoutUser = async (): Promise<LogoutResponse> => {
-    const { data: response } = await mainApi.post<LogoutResponse>(LOGOUT, null, {
+export const logoutUser = async (data:PayloadLogout ): Promise<LogoutResponse> => {
+    const { data: response } = await mainApi.post<LogoutResponse>(LOGOUT, data, {
         withCredentials: true,
     });
     return response;
