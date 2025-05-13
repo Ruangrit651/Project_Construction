4efae82cd91bdf9ab91b7aed2c818cdb910f5053
rueangrit-sri <s65111279@gmail.com>
2025-05-13 13:49:22 +0700

    Update

--- frontend/src/components/layouts/layout.employee.tsx
+++ frontend/src/components/layouts/layout.employee.tsx
@@ -1,21 +1,15 @@
 import { Outlet } from "react-router-dom";
-import NavbarMain from "./navbars/navbar.main";
-import SidebarMain from "./sidebars/sider.employee";
+import NavbarEmployee from "./navbars/navbar.mainEmployee";
 
 const MainLayoutEmployee = () => {
   
   return (
     <div className="min-h-screen flex flex-col">
-      <NavbarMain />
-      <div className="flex flex-1 relative">
-        <div className="w-64 flex-shrink-0">
-          <SidebarMain />
-        </div>
+      <NavbarEmployee />
         <div className="flex-grow p-4 overflow-x-auto">
           <Outlet />
         </div>
       </div>
-    </div>
   );
 };
 
--- frontend/src/components/layouts/layout.manager.tsx
+++ frontend/src/components/layouts/layout.manager.tsx
@@ -1,19 +1,12 @@
 import { Outlet } from "react-router-dom";
 import NavbarMain from "./navbars/navbar.main";
-import SidebarMain from "./sidebars/sider.manager";
 
 const MainLayoutManager = () => {
-  
   return (
     <div className="min-h-screen flex flex-col">
       <NavbarMain />
-      <div className="flex flex-1 relative">
-        <div className="w-64 flex-shrink-0">
-          <SidebarMain />
-        </div>
-        <div className="flex-grow p-4 overflow-x-auto">
-          <Outlet />
-        </div>
+      <div className="flex-grow p-6 overflow-x-auto">
+        <Outlet />
       </div>
     </div>
   );
